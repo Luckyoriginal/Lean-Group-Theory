71458d0cd4a1c98ccdac5559b595304550143847
lucky <luckyorionuser@gmail.com>
2026-07-10 16:52:40 +0200

    macro

--- Main.lean
+++ Main.lean
@@ -13,9 +13,12 @@ class Group (G : Type) where
   mul_inv   : ∀ a : G, mul a (inv a) = one
 
 open Group
+infixl:70 "*" => Group.mul
+postfix:max "⁻¹" => Group.inv
+notation "e" => Group.one
 
 -- Basic Property
-theorem left_cancel {G : Type} [Group G] (a b c : G) (h : mul c a = mul c b) : a= b := by
+theorem left_cancel {G : Type} [Group G] (a b c : G) (h :  c * a = c * b) : a= b := by
   rw [<- one_mul a]
   rw [<- inv_mul c]
   rw [mul_assoc]
@@ -24,7 +27,7 @@ theorem left_cancel {G : Type} [Group G] (a b c : G) (h : mul c a = mul c b) : a
   rw [inv_mul]
   rw [one_mul]
 
-theorem unique_inv {G : Type} [Group G] (a x : G) (h : mul x a = one) : x= inv a := by
+theorem unique_inv {G : Type} [Group G] (a x : G) (h : x * a = e) : x= a⁻¹  := by
   rw [<- mul_one x ]
   rw [<- mul_inv a]
   rw [<- mul_assoc]
